493f506bbb758a21f67b9c7e36925c7f7aa1e2d5
harmainatiq <harmainatiq@gmail.com>
2024-06-20 16:57:58 +0500

    change: improve embeded performance

--- src/app/Components/Performance-explanation.tsx
+++ src/app/Components/Performance-explanation.tsx
@@ -1,16 +1,54 @@
-`use client`;
+'use client'
 
-import React from "react";
+
+import React, { useState,useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import yt from "./../../../public/Images/image 2.svg";
 
+const embedID = "0RKpf3rK57I";
 function Youtube2() {
+
+  const [imageClicked, setImageClicked] = useState(false);
+
+  const onThumbnailClick = () => {
+    setImageClicked(true);
+  };
+
+  useEffect(()=>{
+    const playImg = document.querySelector("#play-button");
+
+    if (playImg) {
+      playImg.addEventListener("click", onThumbnailClick, { once: true });
+    }
+    return () => {
+      if (playImg) {
+        playImg.removeEventListener("click", onThumbnailClick);
+      }
+    };
+  }, []);
+ 
+
   return (
     <div>
-      <div className="lg:px-10 px-6 flex lg:flex-row flex-col-reverse lg:gap-48 gap-3 lg:mb-28 mb-14">
+      <div className="lg:px-10 px-6 flex lg:flex-row flex-col-reverse lg:gap-48 gap-6 lg:mb-28 mb-14">
+        
+        
+      <div className="lg:ml-20 ml-12 w-[36%] h-[650px] border-secondary border-1 relative bg-[darkgray] flex justify-center items-center">
+      {!imageClicked ? (
+        <>
+          <Image
+            src={yt}
+            layout="fill"
+            objectFit="cover"
+            alt="yt thumbnail"
+            priority
+          />
+          <img id="play-button" className="h-[50px] w-[50px] z-[1]" src="http://addplaybuttontoimage.way4info.net/Images/Icons/7.png" alt="play button" />
+        </>
+      ) : (
         <iframe
-          className="lg:pl-24 pl-12 w-[60%] h-[562px] border-secondary border-1"
+          className="w-full h-full border-secondary border-1"
           src="https://www.youtube.com/embed/QPOLrbKI5oQ"
           title="User psychology in Technical Writing Ft. Yael Basford, Senior Technical Writer, Akamai"
           frameBorder="0"
@@ -18,12 +56,25 @@ function Youtube2() {
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
+      )}
+    </div>
+        
+        
+        {/* <iframe
+          className="lg:pl-24 pl-12 w-[60%] h-[562px] border-secondary border-1"
+          src="https://www.youtube.com/embed/QPOLrbKI5oQ"
+          title="User psychology in Technical Writing Ft. Yael Basford, Senior Technical Writer, Akamai"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe> */}
         <div className="lg:py-32 py-10">
           <div className="w-[14%] text-secondary lg:text-2xl text-lg font-bold border-b-4 border-secondary pb-3 mb-4">
             EXPLANATION
           </div>
           <p className="lg:text-5xl text-xl text-white font-semibold lg:leading-normal">
-            Discover the science behind your
+            Discover the science<br></br> behind your
             <span className="text-secondary"> enhanced performance!</span>
           </p>
         </div>
@@ -31,5 +82,6 @@ function Youtube2() {
     </div>
   );
 }
+
 
 export default Youtube2;
--- src/app/Components/Wiesberger-testinomal.tsx
+++ src/app/Components/Wiesberger-testinomal.tsx
@@ -1,11 +1,33 @@
-`use client`;
+'use client'
 
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import yt from "./../../../public/Images/image 2.svg";
 
+const embedID = "0RKpf3rK57I";
 function Youtube1() {
+
+  const [imageClicked, setImageClicked] = useState(false);
+
+  const onThumbnailClick = () => {
+    setImageClicked(true);
+  };
+
+  useEffect(()=>{
+    const playImg = document.querySelector("#play-button");
+
+    if (playImg) {
+      playImg.addEventListener("click", onThumbnailClick, { once: true });
+    }
+    return () => {
+      if (playImg) {
+        playImg.removeEventListener("click", onThumbnailClick);
+      }
+    };
+  }, []);
+ 
+
   return (
     <div>
       <div className="lg:px-10 px-6 flex lg:flex-row flex-col lg:gap-14 gap-3 lg:mb-28 ">
@@ -19,8 +41,22 @@ function Youtube1() {
             testimonial, DP World Tour Pro Player
           </p>
         </div>
+
+        <div className="lg:mr-24 mr-12 w-[65%] h-[562px] border-secondary border-1 relative bg-[darkgray] flex justify-center items-center">
+      {!imageClicked ? (
+        <>
+          <Image
+            src={yt}
+            layout="fill"
+            objectFit="cover"
+            alt="yt thumbnail"
+            priority
+          />
+          <img id="play-button" className="h-[50px] w-[50px] z-[1]" src="http://addplaybuttontoimage.way4info.net/Images/Icons/7.png" alt="play button" />
+        </>
+      ) : (
         <iframe
-          className="lg:pl-24 pr-12 w-[55%] h-[562px] border-secondary border-1"
+          className="w-full h-full border-secondary border-1"
           src="https://www.youtube.com/embed/QPOLrbKI5oQ"
           title="User psychology in Technical Writing Ft. Yael Basford, Senior Technical Writer, Akamai"
           frameBorder="0"
@@ -28,6 +64,19 @@ function Youtube1() {
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
         ></iframe>
+      )}
+    </div>
+
+
+        {/* <iframe
+          className="lg:pl-24 pr-12 w-[55%] h-[562px] border-secondary border-1"
+          src="https://www.youtube.com/embed/QPOLrbKI5oQ"
+          title="User psychology in Technical Writing Ft. Yael Basford, Senior Technical Writer, Akamai"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe> */}
       </div>
     </div>
   );
